7f2e55349a7011ec16b45407c8c6b7eaed883b94
Rockstar4u <155758944+Rockstar4u@users.noreply.github.com>
2024-02-17 20:44:01 +0530

    Delete Main.tf

--- Main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-eastt-1"  # Choose your desired region
-}
-
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Additional resources like Subnets, Security Groups, etc.
